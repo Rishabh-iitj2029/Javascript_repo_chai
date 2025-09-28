218275464bebf9d01923fd3cfdd11051f2748329
Rishabh Raj <rishabhraj456789@gmail.com>
2025-09-28 08:08:11 +0000

    scoping and hoisting

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -12,3 +12,51 @@ if(true){
 console.log("Outer: ",a)
 // console.log(b)
 console.log( c)
+
+
+// Nested scope
+
+function one(){
+    const username = "Ankit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+
+    // console.log(website);
+    two()
+}
+ 
+one()
+
+if (true){
+    const username = "rishabh"
+    if(username === "rishabh"){
+        const website = "youtube"
+        console.log(website + username);
+        
+    }
+
+    // console.log(website)
+
+}
+// console.log(username);
+
+
+// ********************Interesting************************
+
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+
+
+
+// console.log(addTwo(15));
+const addTwo = function(num){
+    return num + 2
+}
+
+
