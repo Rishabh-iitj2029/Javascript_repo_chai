8c4545636aceaa1de6e6a1be01c7a8d7b4d369ad
Rishabh Raj <rishabhraj456789@gmail.com>
2025-10-01 21:29:22 +0000

    forEach loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,50 @@
+const programmingLang =  ["python", "C++", "C", "java", "javascript", "Kotlin"]
+
+// programmingLang.forEach(function (item){
+//     console.log(item);
+    
+// })  
+
+// programmingLang.forEach((item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// programmingLang.forEach(printMe)
+
+// programmingLang.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+const myCoding = [
+    {
+        language: "JavaScript",
+        languageFile: ".js",
+        print: "console"
+    },
+    {
+        language: "C++",
+        languageFile: ".cpp",
+        print: "cout"
+    },
+    {
+        language: "python",
+        languageFile: ".py",
+        print: "print"
+    }
+]
+
+myCoding.forEach( (item) => {
+    for(const key in item){
+        console.log(key, ":", item[key]);
+        console.log("\n");
+        
+    }
+
+    
+})
