a25a41eb89a407701179642f0bb8e93eead164e2
Rishabh Raj <rishabhraj456789@gmail.com>
2025-10-03 21:48:45 +0000

    digital clock

---
+++ 07_Projects/03_Digital_clock/chaiaurcode.js
@@ -0,0 +1,11 @@
+const clock = document.getElementById("clock")
+
+
+
+setInterval(() => {
+    let date = new Date()
+    // console.log(date.toLocaleDateString())
+    // console.log(date.toLocaleTimeString())
+    clock.innerHTML = date.toLocaleTimeString()
+
+}, 2000)
