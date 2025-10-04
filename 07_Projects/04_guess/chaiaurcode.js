3c7ab809a67a0f4650f788f1ece311e2e43f40a7
Rishabh Raj <rishabhraj456789@gmail.com>
2025-10-04 09:46:47 +0000

    guessing number(incomplete)

--- 07_Projects/04_guess/chaiaurcode.js
+++ 07_Projects/04_guess/chaiaurcode.js
@@ -1,49 +1,84 @@
-// const userInput = document.querySelector('.guessField')
-// const form = document.querySelector('.form')
+const randomNumber = Math.floor(Math.random()*100 +1)
+console.log(randomNumber);
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas')
 
-//     const guess = parseInt(userInput.value);
-//     console.log(guess)
-// })
+const p = document.querySelector('p')
 
-// // console.log(4567);
-// const lastElement = document.querySelector('.lastElement')
-// const numGuess = document.createElement('p')
-// numGuess.innerHTML = "5"
-// lastElement.replaceWith(numGuess)
-// console.log(lastElement)
+let preGuess = []
+let numGuess = 1
 
-const form = document.querySelector(".form")
-const userInput = document.querySelector(".guessField")
-const preGuess = document.querySelector(".guesses")
-const lastElement = document.querySelector('.lastElement')
-const lowOrHi = document.querySelector('.lowOrHi')
+let playGame = true
+if(playGame){
+    submit.addEventListener('click', (e) =>{
+        e.preventDefault();
 
-const div = document.createElement('div')
-div.className = "result"
-div.style.color = "white"
+        const guess = parseInt(userInput.value)
+        validateGuess(guess)
+    })
+}
 
-// console.table(form, userInput, preGuess, lastElement, lowOrHi)
-// console.log(lowOrHi);
-
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const guess = parseInt(userInput.value)
-    // console.log(guess)
-
-    const corrNum = 50
-    // console.log(corrNum);
-    const guessNum = 1;
-    
-    for(let i = 1; i<=10; i++, guessNum++){
-        if(guess == corrNum){
-            div.innerHTML = `you guessed it right in ${guessNum} guess`
-            break;
-        }
+function validateGuess(guess){
+    if(guess = '' || guess<0 || guess >100 || isNaN(guess)){
+        alert("please enter a valid number")
+        return false
     }
-})
+    else{
+        preGuess.push(guess)
 
+        if(numGuess == 11){
+            displayMessage(`Game over, the random number was ${randomNumber}`)
+            endGame()
+        }
+
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+        return true
+    }
+}
+
+function checkGuess(guess){
+    if(guess == randomNumber){
+        displayMessage(`you guessed it correctly `)
+        endGame()
+    }
+
+    else if(guess > randomNumber){
+        displayGuess(`Guess is high `)
+    }
+
+    else if(guess < randomNumber){
+        displayMessage(`Guess is low`)
+    }
+}
+
+function displayGuess(guess){
+    userInput.value = ''
+    guessSlot.innerHTML = `${guess}`
+    numGuess++;
+    remaining = `${11-numGuess}`
+}
+function displayMessage(msg){
+    lowOrHi.innerHTML = `<h2>${msg}</h2>`
+}
+
+function newGame(){
+    
+}
+
+function endGame(){
+    userInput.value = ''
+    userInput.setAttribute('disabled', '')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id="newGame"> Start new Game</h2> `
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
+}
